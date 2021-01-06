d1606b8a70c07fae039d240e18a4e2508dad04f6
Chinedu <okoriechinedusunday@gmail.com>
2021-01-06 13:00:35 +0100

    create controller model

---
+++ models/Controller.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const controllerSchema = new mongoose.Schema({
+	htslUniqueCode: {
+		type: String,
+		required: true,
+	},
+	adminFirstName: {
+		type: String,
+	},
+	adminLastName: {
+		type: String,
+	},
+	adminEmail: {
+		type: String,
+	},
+	adminEmail: {
+		type: String,
+	},
+	adminPermanentPassword: {
+		type: String,
+	},
+	adminTemporaryPassword: {
+		type: String,
+	},
+	adminPrivilegeLevel: {
+		type: String,
+	},
+	timeStamp: {
+		type: String,
+	},
+});
+
+const Controller = mongoose.model('Controller', controllerSchema);
+module.exports = User;
