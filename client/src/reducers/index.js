fea10d0f706607ab6921b2365827b9a845086d99
Chinedu <okoriechinedusunday@gmail.com>
2021-01-07 11:34:05 +0100

    create store and root reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import { registerReducer } from './registerReducer';
+// import { oauthReducer } from './oauthReducer';
+// import { dashboardReducers } from './dashboardReducers';
+
+export default combineReducers({
+	// registerReducer,
+	// oauthReducer,
+	// dashboardReducers,
+});
